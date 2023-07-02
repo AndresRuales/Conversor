2ca8e9be1aa6cef026f41e0806366343ac21d81b
Andres Ruales <danielandresruales@hotmail.com>
2023-07-01 21:28:42 -0500

    Se hace una prueba con la clase Scanner

--- src/RU/ConversorMoneda/TestMain.java
+++ src/RU/ConversorMoneda/TestMain.java
@@ -1,13 +1,35 @@
 package RU.ConversorMoneda;
 
+import java.util.Scanner;
+
 public class TestMain {
+	
+	/*- Convertir de la moneda de tu país a Dólar
+    - Convertir de la moneda de tu país  a Euros
+    - Convertir de la moneda de tu país  a Libras Esterlinas
+    - Convertir de la moneda de tu país  a Yen Japonés
+    - Convertir de la moneda de tu país  a Won sul-coreano
+	*/
 	
 	public static void main(String[] args) {
 		
+		//BORRADOR DE COMO FUNCIONARÍA NUESTRO CONVERSOR
 		
+		//Se crea objeto de tipo Scanner que guarda la moneda del usuario
+		Scanner entradaMoneda = new Scanner(System.in);
 		
+		//Solicita al usuario texto.
+		System.out.println("Ingrese el valor de la moneda a convertir: ");
+		Float moneda = entradaMoneda.nextFloat();
 		
+		System.out.println("Digito de moneda ingresada: "+moneda);
+
 		
+		//Se cierra el objeto moneda para  liberar los 
+			//	recursos asociados a la entrada estándar del sistema.
+		entradaMoneda.close();
+		
+		System.out.println("Fin del programa");
 	}
 
 }
